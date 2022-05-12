6657039f8b96a1fde1c997d7cb21208992490785
yzhao <ying.zhao@sfchronicle.comm>
2022-05-12 11:53:55 -0700

    small tweat to the helper

--- youtube_captions.py
+++ youtube_captions.py
@@ -4,6 +4,7 @@ import pandas as pd
 import requests
 import re
 import json
+from helper import *
 
 df = pd.read_csv('GoogleAds/CA.csv')
 df = df[df['ad_type'] == 'Video'].reset_index(drop = True) # only get text ads
@@ -22,21 +23,13 @@ for url in urls:
     youtube_urls.append(youtube_url)
     # assigning srt variable with the list
     # of dictonaries obtained by the get_transcript() function
-    # subs = YouTubeTranscriptApi.get_transcript(youtube_id)
-    # #prints the result
-    # list = []
-    # for sub in subs:
-    #   list.append(" " + sub['text'])
-    # captions = ""
-    # for item in list:
-    #   captions += item
-    # youtube_captions.append(captions)
-
-youtube_urls.pop(5)
-print(youtube_urls)
-    
-
-df_youtube = pd.DataFrame(youtube_urls).rename(columns = {0:'youtube_url'})
-df = pd.concat([df_url,df_youtube],axis=1)
-df['platform'] = 'google'
+    subs = YouTubeTranscriptApi.get_transcript(youtube_id)
+    #prints the result
+    list = []
+    for sub in subs:
+      list.append(" " + sub['text'])
+    captions = ""
+    for item in list:
+      captions += item
+    youtube_captions.append(captions)
 
